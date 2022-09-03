cf3d742950564a01680965adcaae32a5dc281b00
Jan Zisenis <jan.zisenis@gmail.com>
2022-09-03 16:21:20 +0200

    test change
    Signed-off-by: Jan Zisenis <jan.zisenis@gmail.com>

--- src/api/main.py
+++ src/api/main.py
@@ -4,8 +4,19 @@ from api.tutorial.database import engine
 from mangum import Mangum
 from api.tutorial.routers import tutorial, user, authentication, vote
 from api.tutorial.routers import video, topic, web_tutorial
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 models.Base.metadata.create_all(engine)
 
